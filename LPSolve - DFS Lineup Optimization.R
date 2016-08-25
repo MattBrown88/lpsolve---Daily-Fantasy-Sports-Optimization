48932df1ea5dfeb195d5537947a3d194ae4e299f
MattBrown88 <matthew.brown.iowa@gmail.com>
2016-08-25 10:41:48 -0400

    Update LPSolve - DFS Lineup Optimization.R

--- LPSolve - DFS Lineup Optimization.R
+++ LPSolve - DFS Lineup Optimization.R
@@ -1,5 +1,5 @@
 ####Generate optimal daily fantasy baseball line up using lpsolve function####
-
+#Install.packages("lpsolve")
 
 library(lpSolve)
 
@@ -11,14 +11,18 @@ dataset$position <- as.factor(dataset$position)
 dataset$salary <-as.numeric(dataset$salary)
 
 
-#### Prepare constraint matrix #####
+#### Prepare constraint matrix of zeros #####
 A <- matrix(0, nrow = 8, ncol = nrow(dataset))
 
-#1B
+#Designate the positions that are equivalent to each other when generating the optimal lineup
+#There are 7 distinct positions and 1 constraint in which salary is < 50,000
+#I.e. A player with the position 1B/2B can fill the 1B or the 2B position
+#Add a "1" to all position that can fill that position slot
+
+
+#Set 1B parameters
 j<-1
 i<-1
-
-#Designate the positions that are equivalent to each other when generating the optimal lineup
 for (i in 1:nrow(dataset)){
   if (dataset$position[i]=="1B"    || 
       dataset$position[i]=="1B/2B" || 
@@ -105,16 +109,15 @@ sol <- lp(direction = "max", objective.in, # maximize objective function
           const.mat, const.dir, const.rhs,   # constraints
           all.bin = TRUE)                    # use binary variables only
 
-# View the solution
-
+### View the solution
 inds <- which(sol$solution == 1)
 sum(dataset$salary[inds])
 
 
-solution1<-dataset[inds, ]
+solution<-dataset[inds, ]
 
 #Print players in optimal lineup
-solution1
+solution
 
 #Write csv file of the optimal lineup
 write.table(solution1, "mydata.txt", sep="\t")
